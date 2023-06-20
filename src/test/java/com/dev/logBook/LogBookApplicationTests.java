ae25a31658e71179bc641b7ba10ac755d37795a8
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-06-20 15:21:01 -0300

    initial commit

---
+++ src/test/java/com/dev/logBook/LogBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.logBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
